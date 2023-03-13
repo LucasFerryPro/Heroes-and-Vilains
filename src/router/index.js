d80351951aa2098325ef0c5b3c1769a615df676e
Maelttt <mael.turchetto@orange.fr>
2023-03-13 15:44:02 +0100

    pardon lucas

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,39 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
-    path: '/auth',
-    name: 'authentification',
-    component: () => import('../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../components/LoginComponent.vue')
+  },
+  {
+    path: '/organizations',
+    name: 'Organizations',
+    component: () => import(/* webpackChunkName: "orgs" */ '../components/OrgsComponent.vue')
+  },
+  {
+    path: '/organizations/:id',
+    name: 'Organization Details',
+    component: () => import(/* webpackChunkName: "org" */ '../components/OrgComponent.vue')
+  },
+  {
+    path: '/teams',
+    name: 'Teams',
+    component: () => import(/* webpackChunkName: "teams" */ '../components/TeamsComponent.vue')
+  },
+  {
+    path: '/teams/:id',
+    name: 'Team Details',
+    component: () => import(/* webpackChunkName: "team" */ '../components/TeamComponent.vue')
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
